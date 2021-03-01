b70f80103439ec7b8a6e18bb9e0ccbf200254392
Lori Lynn Aoki <66411788+LoriLynn777@users.noreply.github.com>
2021-03-01 00:34:58 -1000

    clickEvents

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello there');
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Zatoichi', e)}>Click me again</button>
         </div>
-      );
+    );
 }
  
 export default Home;
