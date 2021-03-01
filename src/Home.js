073310ae31f3629b726dc7d3523bc451229cbee3
Lori Lynn Aoki <66411788+LoriLynn777@users.noreply.github.com>
2021-03-01 02:27:20 -1000

    useEffectHook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -11,6 +11,10 @@ const Home = () => {
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect');
+    });
 
     return (
         <div className="home">
