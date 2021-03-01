e99f1995d080a37d5af079724c66b2b3981a6e28
Lori Lynn Aoki <66411788+LoriLynn777@users.noreply.github.com>
2021-03-01 03:23:16 -1000

    conditionalLoadingmessage

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,27 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
+    const [isPending, setIsPending] = useState(true);
 
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
             });
+        }, 1000);
     }, []); 
 
     return (
         <div className="home">
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" /> */}
-            {/* <button onClick={() => setName('luigi')}>Change Name</button>
-            <p>{ name }</p> */}
+
         </div>
     );
 }
