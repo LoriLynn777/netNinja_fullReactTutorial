1c186a15a56f9fe80f9a03317eb21e91f9a3e91b
Lori Lynn Aoki <66411788+LoriLynn777@users.noreply.github.com>
2021-03-01 02:35:50 -1000

    useEffectDependencies

--- src/Home.js
+++ src/Home.js
@@ -8,18 +8,23 @@ const Home = () => {
         { title: 'Web dev top tips', body; 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);setBlogs(newBlogs);
     }
 
     useEffect(() => {
-        console.log('use effect');
-    });
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
+            <button onClick={() => setName('luigi')}>Change Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
